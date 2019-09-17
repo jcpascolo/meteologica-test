a84c971fa727ce61a2a939a4adfecec60b8a550e
jkp <jkp@localhost.localdomain>
2019-09-17 21:43:40 +0200

    finishing details

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let path = require("path");
 let serveStatic = require("serve-static");
 
 app = express();
-app.use(serveStatic(__dirname + "/build"));
+app.use(serveStatic(__dirname + "/dist"));
 let port = process.env.PORT || 8080;
 app.listen(port);
 console.log("server started " + port);
