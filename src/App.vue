c572cdfac665090e6df85cf982e5e25623f45a91
jkp <jkp@localhost.localdomain>
2019-09-17 22:56:15 +0200

    loading the datas from the yaml file

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">APP</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+let datas = require("json-loader!yaml-loader!../data.yml");
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  mounted() {
+    try {
+      console.log(datas.power.values[0]);
+    } catch (e) {
+      console.log(e);
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
