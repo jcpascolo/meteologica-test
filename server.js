6811747ebf4d09f2a9a179b4e17adab13a11d1ea
jkp <jkp@localhost.localdomain>
2019-09-22 18:34:05 +0200

    trying to fix deploy errors

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ let serveStatic = require("serve-static");
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 5000;
 app.listen(port);
-console.log("server started " + port);
