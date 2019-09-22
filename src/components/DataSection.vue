ed3ba9e13bec38888ef135eeb303df6a02f20f42
jkp <jkp@localhost.localdomain>
2019-09-22 19:04:28 +0200

    trying deployment again

--- src/components/DataSection.vue
+++ src/components/DataSection.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="d-flex justify-content-center">
     <div>
-      <h1>Last Recibed Data</h1>
+      <h5>Last Recibed Data</h5>
       <div>Time: {{lastData.time}}</div>
       <div>Original Data: {{lastData.originalData.toFixed(2)}}{{startUnit}}</div>
       <div>Transformed Data: {{lastData.transformedData.toFixed(2)}}{{endUnit}}</div>
 
-      <h1>Last Minute Average Recibed Data</h1>
+      <h5>Last Minute Average Recibed Data</h5>
       <div>Time: {{averageMinuteData.time}}</div>
       <div>Original Data: {{averageMinuteData.originalData.toFixed(2)}}{{startUnit}}</div>
       <div>Transformed Data: {{averageMinuteData.transformedData.toFixed(2)}}{{endUnit}}</div>
